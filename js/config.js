1256a38c8bc67c367c6011701b2bffb29ef968e6
mehmet karsu <80062501+mehmetkarasu@users.noreply.github.com>
2025-09-09 19:42:55 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 // Bu örnek, bir üçüncü taraf API'ye yapılan isteklerde açık şekilde API anahtarının kullanıldığını göstermektedir.
 
-const API_KEY = "sk_test_51NB...iCOT9l"; // ⚠️ HATALI: API anahtarı açıkta!
+const API_KEY = "sk_test_51NB1234567890ABCDEFGHiCOT9l"; // ⚠️ HATALI: API anahtarı açıkta!
 
 function fetchUserData(userId) {
   const url = `https://api.thirdpartyservice.com/users/${userId}?apikey=${API_KEY}`;
